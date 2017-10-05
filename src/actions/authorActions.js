9730e7e82195eb9fcba3c1fab52e3ba1a8967d79
YonePC <YonePC@YONE>
2017-10-05 17:45:40 +0100

    Adding flux design pattern 2: Created an authorStore to get payloads and emit events to React components Added object-assign library to make an object from passing in two objects and a prototype to let herency work as ES6 Added listeners and a emit to support authorStore's changes

--- src/actions/authorActions.js
+++ src/actions/authorActions.js
@@ -1,13 +1,14 @@
-"use stric";
+"use strict";
 
 var Dispatcher = require('../dispatcher/appDispatcher');
 var AuthorApi = require('../api/authorApi');
-var ActionTypes = require('../contants/ActionTypes');
+var ActionTypes = require('../constants/actionTypes');
 
 var AuthorActions = {
-	createAuthor : function(author) {
+	createAuthor: function(author) {
 		var newAuthor = AuthorApi.saveAuthor(author);
 
+		//Hey dispatcher, go tell all the stores that an author was just created.
 		Dispatcher.dispatch({
 			actionType: ActionTypes.CREATE_AUTHOR,
 			author: newAuthor
